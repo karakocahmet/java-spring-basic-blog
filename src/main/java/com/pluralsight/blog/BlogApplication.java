786e047627a4ec759431e075a5595731f96e6ad5
ahmet karakoc <akarakoc2@sgk.gov.tr>
2022-10-16 13:34:22 +0300

    odev1

--- src/main/java/com/pluralsight/blog/BlogApplication.java
+++ src/main/java/com/pluralsight/blog/BlogApplication.java
@@ -1,5 +1,6 @@
 package com.pluralsight.blog;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
--- src/main/java/com/pluralsight/blog/BlogController.java
+++ src/main/java/com/pluralsight/blog/BlogController.java
@@ -1,8 +1,18 @@
 package com.pluralsight.blog;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class BlogController {
+
+    @RequestMapping("/")
+    public String listPosts()
+    {
+        ModelMap modelmap = new ModelMap();
+        modelmap.put("title","deneme title");
+        return "home";
+    }
 
 }
